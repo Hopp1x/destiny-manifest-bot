4d39834e706ef4683e158d5060b7fda57ad1335b
Richard Chan <150969811+Hopp1x@users.noreply.github.com>
2024-02-17 20:56:58 -0500

    Shortened property names in the Manifest JSON

--- src/update-destiny-manifest.ts
+++ src/update-destiny-manifest.ts
@@ -4,9 +4,27 @@ import admZip from "adm-zip";
 import {
   getDestinyManifest,
   getDestinyManifestSlice,
+  DestinyDamageTypeDefinition,
+  DestinyInventoryItemDefinition,
+  DestinyPlugSetDefinition,
+  DestinySeasonDefinition,
+  DestinyStatDefinition,
+  DestinyStatGroupDefinition, 
   HttpClientConfig
 } from "bungie-api-ts/destiny2";
 import fse from "fs-extra";
+
+interface DestinyManifest {
+  DamageType?: { [hash: number]: DestinyDamageTypeDefinition },
+  InventoryItem?: { [hash: number]: DestinyInventoryItemDefinition },
+  PlugSet?: { [hash: number]: DestinyPlugSetDefinition },
+  Season?: { [hash: number]: DestinySeasonDefinition },
+  Stat?: { [hash: number]: DestinyStatDefinition },
+  StatGroup?: { [hash: number]: DestinyStatGroupDefinition } 
+};
+
+// Http client for Bungie API endpoints that (may) require a API key
+const authHttpClient = createHttpClient(process.env.BUNGIE_API_KEY!);
 
 // Http client for Bungie API requests where you should not include a API Key
 async function httpClient(config: HttpClientConfig) {
@@ -17,10 +35,7 @@ async function httpClient(config: HttpClientConfig) {
     method: config.method
   });
   return await response.json();
-}
-
-// Http client for Bungie API endpoints that (may) require a API key
-const authHttpClient = createHttpClient(process.env.BUNGIE_API_KEY!);
+};
 
 async function run() {
   console.log("Fetching Destiny Manifest metadata...");
@@ -33,7 +48,7 @@ async function run() {
   }
   // Retrieve the updated manifest components.
   console.log("Fetching Destiny Manifest components...");
-  const destinyManifest = await getDestinyManifestSlice(httpClient, {
+  const manifestComponents = await getDestinyManifestSlice(httpClient, {
     destinyManifest: manifestMetadata.Response,
     tableNames: [
       "DestinyDamageTypeDefinition",
@@ -45,7 +60,14 @@ async function run() {
     ],
     language: "en"
   });
-  
+  const destinyManifest: DestinyManifest = {
+    DamageType: manifestComponents.DestinyDamageTypeDefinition,
+    InventoryItem: manifestComponents.DestinyInventoryItemDefinition,
+    PlugSet: manifestComponents.DestinyPlugSetDefinition,
+    Season: manifestComponents.DestinySeasonDefinition,
+    Stat: manifestComponents.DestinyStatDefinition,
+    StatGroup: manifestComponents.DestinyStatGroupDefinition
+  };
   // Write the updated Destiny Manifest to the repository.
   console.log("Writing new manifest definitions to 'manifest.zip'...");
   const zip = new admZip();
@@ -56,6 +78,6 @@ async function run() {
   console.log("Writing new repository README...");
   await fse.writeFile("README.md", `# d2-manifest-bot\nA GitHub Action for fetching the latest version of Bungie's Destiny Manifest.\n## Current manifest version: "${manifestMetadata.Response.version}"`);
   console.log("Uploading changed files to repository...");
-}
+};
 
 await run();
